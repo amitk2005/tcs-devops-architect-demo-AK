fc4ca5f9436dce948be40a17b55ac691d8cb5b52
Amit Koranne <amit_koranne2005@outlook.com>
2025-11-23 23:56:37 +0530

    initial commit

---
+++ src/main/java/com/tcs/tcs_devops_architect_AK/TcsDevopsArchitectAkApplication.java
@@ -0,0 +1,21 @@
+package com.tcs.tcs_devops_architect_AK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TcsDevopsArchitectAkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsDevopsArchitectAkApplication.class, args);
+	}
+	
+	
+	@GetMapping("/")
+	public String home() {
+		return "<h1>Welcome to TCS DevOps Architect Demo Application<h1>";
+	}
+}
---
+++ src/test/java/com/tcs/tcs_devops_architect_AK/TcsDevopsArchitectAkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.tcs_devops_architect_AK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcsDevopsArchitectAkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
